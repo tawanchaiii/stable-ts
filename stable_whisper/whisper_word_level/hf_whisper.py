13c2a6f2817ae0f4ad00b86f56170afe14cad04b
Champ Tawanchai <tawanchai_champ@hotmail.com>
2024-08-23 19:42:40 +0700

    Change model name on hf_whisper

--- stable_whisper/whisper_word_level/hf_whisper.py
+++ stable_whisper/whisper_word_level/hf_whisper.py
@@ -21,8 +21,8 @@ HF_MODELS = {
     "large-v2": "openai/whisper-large-v2",
     "large-v3": "openai/whisper-large-v3",
     "large": "openai/whisper-large-v3",
-    "thonburi:medium": "biodatlab/whisper-th-medium-combined",
-    "thonburi:large-v3": "biodatlab/whisper-th-large-v3-combined",
+    "thonburi_medium": "biodatlab/whisper-th-medium-combined",
+    "thonburi_large-v3": "biodatlab/whisper-th-large-v3-combined",
 }
 
 
